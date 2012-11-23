7f88b2067b604312bb12007c88bb484e4e756eff
Koko Tas <cfh1510@yahoo.gr>
2012-11-23 21:04:25 +0200

    Added checks for cLK. 2nd Activity is not shown in launcher app-list anymore.

--- gen/com/hd2/alarm_clk/R.java
+++ gen/com/hd2/alarm_clk/R.java
@@ -41,13 +41,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f060000;
-        public static final int button1=0x7f060003;
-        public static final int button2=0x7f060004;
-        public static final int button3=0x7f060005;
+        public static final int button1=0x7f060005;
+        public static final int button2=0x7f060006;
+        public static final int button3=0x7f060007;
+        public static final int menu_item_0=0x7f060002;
+        public static final int menu_item_1=0x7f060003;
         public static final int menu_settings=0x7f060001;
-        public static final int textview1=0x7f060006;
-        public static final int textview2=0x7f060007;
-        public static final int title_activity_alarm_clk=0x7f060002;
+        public static final int textview1=0x7f060008;
+        public static final int textview2=0x7f060009;
+        public static final int title_activity_alarm_clk=0x7f060004;
     }
     public static final class style {
         public static final int AppTheme=0x7f080000;
--- src/com/hd2/alarm_clk/AlarmClk.java
+++ src/com/hd2/alarm_clk/AlarmClk.java
@@ -18,28 +18,114 @@
  */
 package com.hd2.alarm_clk;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeoutException;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.text.SpannableString;
 import android.text.format.Time;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.stericson.RootTools.Command;
+import com.stericson.RootTools.RootTools;
+
 public class AlarmClk extends Activity {
+	private final int ID_MENU_INF = 0;
+	private final int ID_MENU_EXIT = 1;
 	TimePicker tp;
 	PowerManager pm;
+	
+	boolean clk = false;
+	int clk_ver;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        
         setContentView(R.layout.activity_alarm_clk);
+        RootTools.debugMode = false;
+        
+        // Root?
+        if (RootTools.isRootAvailable()) {
+        	if (RootTools.isAccessGiven()) {
+        		//Check bootloader
+        		clk = false;
+        		Command get_bldr = new Command(0, "sed -n 's/clk/&/p' /proc/cmdline")
+            	{
+        			@Override
+        	        public void output(int id, String line)
+        	        {
+        				if(line.contains("clk=")) {
+        					clk = true;
+        					String ver = line.substring(line.indexOf("clk=") + 4, line.length()).trim().replace(".", "");
+        					if(ver.length() == 2) {
+        						ver = ver + "00";
+        					}
+        					else if(ver.length() == 3) {
+        						ver = ver + "0";
+        					}
+        					clk_ver = Integer.decode(ver);
+        				}
+        	        }
+            	};
+        		try {
+    				RootTools.getShell(true).add(get_bldr).waitForFinish();
+    			} catch (InterruptedException e) {
+    				e.printStackTrace();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			} catch (TimeoutException e) {
+    				e.printStackTrace();
+    			}
+        		if(clk) {
+        			if(clk_ver < 1514) {
+        				AlertDialog.Builder WrongVersion = new AlertDialog.Builder(this);
+        				WrongVersion.setMessage("Current version of cLK is outdated!\nAlarmClk is compatible with version higher than 1.5.1.4");
+        				WrongVersion.setPositiveButton("Exit",
+                								new DialogInterface.OnClickListener() {public void onClick(DialogInterface arg0, int arg1) {finish();}}
+                								);
+        				WrongVersion.show();
+        			}
+        		} else {
+        			AlertDialog.Builder NotCLK = new AlertDialog.Builder(this);
+        			NotCLK.setMessage("AlarmClk is compatible ONLY with cLK bootloader");
+        			NotCLK.setPositiveButton("Exit",
+            								new DialogInterface.OnClickListener() {public void onClick(DialogInterface arg0, int arg1) {finish();}}
+            								);
+        			NotCLK.show();
+        		}        		
+        	} else {
+        		AlertDialog.Builder NoRoot = new AlertDialog.Builder(this);
+        		NoRoot.setMessage("Root access could not be obtained. Please check whether your device is rooted, or restart the application to try again.");
+        		NoRoot.setPositiveButton("Exit",
+        								new DialogInterface.OnClickListener() {public void onClick(DialogInterface arg0, int arg1) {finish();}}
+        								);
+            	NoRoot.show();
+        	}
+        } else {
+        	AlertDialog.Builder NoRoot = new AlertDialog.Builder(this);
+    		NoRoot.setMessage("Root access could not be obtained. Please check whether your device is rooted, or restart the application to try again.");
+    		NoRoot.setPositiveButton("Exit",
+    								new DialogInterface.OnClickListener() {public void onClick(DialogInterface arg0, int arg1) {finish();}}
+    								);
+        	NoRoot.show();
+        }
+        
         Time now = new Time();
     	now.setToNow();
     	tp = (TimePicker)this.findViewById(R.id.timePicker1);
@@ -51,10 +137,42 @@ public class AlarmClk extends Activity {
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_alarm_clk, menu);
+    	menu.add(Menu.NONE, ID_MENU_INF, Menu.NONE, R.string.menu_item_0);
+    	menu.add(Menu.NONE, ID_MENU_EXIT, Menu.NONE, R.string.menu_item_1);
         return true;
     }
-
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if(item.getItemId() == ID_MENU_INF)	{
+    		final TextView link = new TextView(this);
+         	final SpannableString s = new SpannableString("This is an experimental Alarm Clock\n\nYou must have cLK > 1_5_1_4 and\nrunning a patched kernel.\n\nMore info at xda thread:\nforum.xda-developers.com/showthread.php?t=1990111");
+        	Linkify.addLinks(s, Linkify.WEB_URLS);
+        	link.setText(s);
+        	link.setMovementMethod(LinkMovementMethod.getInstance());
+        	
+    		AlertDialog.Builder info = new AlertDialog.Builder(this);
+    		info.setInverseBackgroundForced(true);
+    		info.setTitle("Info");
+    		info.setIcon(android.R.drawable.ic_dialog_info);
+    		info.setView(link);
+    		info.setCancelable(false);
+    		info.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface arg0, int arg1) {
+    				arg0.cancel();
+    				}
+    			}
+    		);
+        	info.show();
+    		return true;
+    	}
+    	else if(item.getItemId() == ID_MENU_EXIT) {
+    		this.finish();
+    		return true;
+    	}
+		return false;    
+    }
+    
     public void SetAlarm(View view) {
     	// Current time
     	Time now = new Time();
@@ -74,9 +192,9 @@ public class AlarmClk extends Activity {
     		if (mintosusp < 0) {
 			   	mintosusp = mintosusp + 1440;//zzz
     		}    
-//    		if (mintosusp > 255) {
-//    			makeToast("Alarm time for more than 255 min from now\n is not supported.");
-//    		} else {
+    		if (clk_ver == 1514 && mintosusp > 255) {
+    			makeToast("Alarm time for more than 255 min from now\n is not supported.");
+    		} else {
 	    		// cLK will use the MinutesToSuspend to suspend device for that time
 	        	final int MinutesToSuspend = mintosusp;
 	    		
@@ -99,42 +217,44 @@ public class AlarmClk extends Activity {
 	    			public void run() { 
 	            		handler.post(new Runnable() { 
 	            			public void run() { 
-	            				/*
-	            			     * Here we reboot using "oem-" prefix in boot reason
-	            			     * This way, the kernel will pass the MinutesToSuspend
-	            			     * to bootloader so that it will detect we want to suspend device
-	            			     * #############################################################
-	            			     * # quoted from arch\arm\mach-msm\pm.c                        #
-	            			     * # if (!strncmp(cmd, "oem-", 4)) {                           #
-	            				 * # 	unsigned code = simple_strtoul(cmd + 4, 0, 16) & 0xff; #
-	            				 * #	restart_reason = 0x6f656d00 | code;                    #
-	            				 * # }                                                         #
-	            				 * #############################################################
-	            				 * 
-	            				 * Problem is that we can't detect correctly the MinutesToSuspend
-	            				 * if it is larger that 255 min.
-	            				 */
-	            				//pm.reboot("oem-" + MinutesToSuspend);
-	            				/*
-	            				 * Workaround would be to add a new reboot_reason
-	            				 * in arch\arm\mach-msm\pm.c. Something like this maybe:
-	            			     * ###############################################################
-	            			     * # if (!strncmp(cmd, "S", 1)) {                           	 #
-	            				 * # unsigned code = simple_strtoul(cmd + 1, 0, 16) & 0x00ffffff;#
-	            				 * #	restart_reason = 0x6f656000 | code;                      #
-	            				 * # }                                                           #
-	            				 * ###############################################################
-	            				 * and then calling here: 
-	            				 * pm.reboot("S" + MinutesToSuspend);
-	            			     */
-	            				pm.reboot("S" + MinutesToSuspend);
+	            				if (clk_ver == 1514) {
+		            				/*
+		            			     * Here we reboot using "oem-" prefix in boot reason
+		            			     * This way, the kernel will pass the MinutesToSuspend
+		            			     * to bootloader so that it will detect we want to suspend device
+		            			     * #############################################################
+		            			     * # quoted from arch\arm\mach-msm\pm.c                        #
+		            			     * # if (!strncmp(cmd, "oem-", 4)) {                           #
+		            				 * # 	unsigned code = simple_strtoul(cmd + 4, 0, 16) & 0xff; #
+		            				 * #	restart_reason = 0x6f656d00 | code;                    #
+		            				 * # }                                                         #
+		            				 * #############################################################
+		            				 * 
+		            				 * Problem is that we can't detect correctly the MinutesToSuspend
+		            				 * if it is larger that 255 min.
+		            				 */
+		            				pm.reboot("oem-" + MinutesToSuspend);
+	            				} else {
+		            				/*
+		            				 * Workaround is to add a new reboot_reason
+		            				 * in arch\arm\mach-msm\pm.c:
+		            			     * ###############################################################
+		            			     * # if (!strncmp(cmd, "S", 1)) {                           	 #
+		            				 * # unsigned code = simple_strtoul(cmd + 1, 0, 16) & 0x00ffffff;#
+		            				 * #	restart_reason = 0x6f656000 | code;                      #
+		            				 * # }                                                           #
+		            				 * ###############################################################
+		            			     */
+		            				pm.reboot("S" + MinutesToSuspend);	            		        	
+	            		        }
+	            				
 	            				finish();
 	                        } 
 	                    }); 
 	                } 
 	            }, 3000);
 	    		
-//    		}
+    		}
     	}
     	
     }
